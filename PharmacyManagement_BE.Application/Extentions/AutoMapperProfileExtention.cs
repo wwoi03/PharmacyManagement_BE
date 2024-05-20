59277101f9f8f1088c4c323fa40b995e7e17f67e
wwoi03 <tuandao8826848@gmail.com>
2024-05-20 11:10:55 +0700

    docs: init automapper

---
+++ PharmacyManagement_BE.Application/Extentions/AutoMapperProfileExtention.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using PharmacyManagement_BE.Application.DTOs.Responses;
+using PharmacyManagement_BE.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PharmacyManagement_BE.Application.Extentions
+{
+    public class AutoMapperProfileExtention : Profile
+    {
+        public AutoMapperProfileExtention()
+        {
+            CreateMap<Product, ViewAllProductQueryResponse>();
+        }
+    }
+}
---
+++ PharmacyManagement_BE.Application/Queries/ProductFeatures/Handlers/GetAllProductQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using PharmacyManagement_BE.Application.DTOs.Requests;
+using PharmacyManagement_BE.Application.DTOs.Responses;
+using PharmacyManagement_BE.Infrastructure.Common.ResponseAPIs;
+using PharmacyManagement_BE.Infrastructure.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PharmacyManagement_BE.Application.Queries.ProductFeatures.Handlers
+{
+    internal class GetAllProductQueryHandler : IRequestHandler<GetAllProductQueryRequest, ResponseAPI<ViewAllProductQueryResponse>>
+    {
+        private readonly IPMEntities _entities;
+
+        public GetAllProductQueryHandler(IPMEntities entities)
+        {
+            this._entities = entities;
+        }
+
+        public async Task<ResponseAPI<ViewAllProductQueryResponse>> Handle(GetAllProductQueryRequest request, CancellationToken cancellationToken)
+        {
+            var products = await _entities.ProductService.GetAll();
+
+            throw new NotImplementedException();
+        }
+    }
+}
