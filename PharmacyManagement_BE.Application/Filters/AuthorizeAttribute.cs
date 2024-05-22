02cc7328b8b7da6ffa54b955f24b650e79abce14
wwoi03 <tuandao8826848@gmail.com>
2024-05-22 09:08:08 +0700

    feat: user authorization

---
+++ PharmacyManagement_BE.Application/Filters/AuthorizeAttribute.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PharmacyManagement_BE.Application.Filters
+{
+    public class AuthorizeAttribute : TypeFilterAttribute
+    {
+        public AuthorizeAttribute() : base(typeof(PMAuthorizeActionFilter))
+        {
+
+        }
+
+        public AuthorizeAttribute(string functionCode, string permission) : base(typeof(PMAuthorizeActionFilter))
+        {
+            Arguments = new object[] { functionCode, permission };
+        }
+    }
+}
