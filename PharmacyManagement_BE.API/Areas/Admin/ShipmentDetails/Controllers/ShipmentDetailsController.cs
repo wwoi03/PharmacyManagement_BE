a5b0b6b8c272e844eed3ac958082efc47d2be8c2
wwoi03 <tuandao8826848@gmail.com>
2024-06-08 14:46:50 +0700

    feat: done get get shipment details by shipment
    Cần xem lại Product Image

---
+++ PharmacyManagement_BE.API/Areas/Admin/ShipmentDetails/Controllers/ShipmentDetailsController.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PharmacyManagement_BE.Application.Queries.ShipmentDetailsFeatures.Requests;
+
+namespace PharmacyManagement_BE.API.Areas.Admin.ShipmentDetails.Controllers
+{
+    [Route("api/[area]/[controller]")]
+    [ApiController]
+    [Area("Admin")]
+    [ApiExplorerSettings(GroupName = "Admin")]
+    public class ShipmentDetailsController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public ShipmentDetailsController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpPost("GetShipmentDetailsByShipment")]
+        public async Task<IActionResult> GetShipmentDetailsByShipment(GetShipmentDetailsByShipmentQueryRequest request)
+        {
+            try
+            {
+                var result = await mediator.Send(request);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
