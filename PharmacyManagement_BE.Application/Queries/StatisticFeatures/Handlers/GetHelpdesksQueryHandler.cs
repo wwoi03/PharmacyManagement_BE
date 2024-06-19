f3191c51736cf3cf59f06e721f7248468ae2ba68
AnhHao083 <nguyenanhhao08032003@gmail.com>
2024-06-19 22:09:51 +0700

    Feats
    Statistic order, revenue

---
+++ PharmacyManagement_BE.Application/Queries/StatisticFeatures/Handlers/GetHelpdesksQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using PharmacyManagement_BE.Application.Queries.StatisticFeatures.Requests;
+using PharmacyManagement_BE.Infrastructure.Common.ResponseAPIs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PharmacyManagement_BE.Application.Queries.StatisticFeatures.Handlers
+{
+    internal class GetHelpdesksQueryHandler : IRequestHandler<GetHelpdesksQueryRequest, ResponseAPI<string>>
+    {
+        public Task<ResponseAPI<string>> Handle(GetHelpdesksQueryRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PharmacyManagement_BE.Application/Queries/StatisticFeatures/Requests/GetHelpdesksQueryRequest.cs
@@ -0,0 +1,14 @@
+﻿using PharmacyManagement_BE.Infrastructure.Common.ResponseAPIs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace PharmacyManagement_BE.Application.Queries.StatisticFeatures.Requests
+{
+    public class GetHelpdesksQueryRequest : IRequest<ResponseAPI<string>>
+    {
+    }
+}
