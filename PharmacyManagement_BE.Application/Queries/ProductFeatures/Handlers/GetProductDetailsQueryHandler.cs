5311260c691b19008e8b9ec22eb3b7c32395bab2
wwoi03 <tuandao8826848@gmail.com>
2024-06-29 21:19:58 +0700

    feat: in process get product details

---
+++ PharmacyManagement_BE.Application/Queries/ProductFeatures/Handlers/GetProductDetailsQueryHandler.cs
@@ -0,0 +1,38 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using PharmacyManagement_BE.Application.Queries.ProductFeatures.Requests;
+using PharmacyManagement_BE.Infrastructure.Common.DTOs.ProductDTOs;
+using PharmacyManagement_BE.Infrastructure.Common.ResponseAPIs;
+using PharmacyManagement_BE.Infrastructure.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PharmacyManagement_BE.Application.Queries.ProductFeatures.Handlers
+{
+    internal class GetProductDetailsQueryHandler : IRequestHandler<GetProductDetailsQueryRequest, ResponseAPI<DetailsProductDTO>>
+    {
+        private readonly PMEntities _entities;
+
+        public GetProductDetailsQueryHandler(PMEntities entities)
+        {
+            this._entities = entities;
+        }
+
+        public async Task<ResponseAPI<DetailsProductDTO>> Handle(GetProductDetailsQueryRequest request, CancellationToken cancellationToken)
+        {
+            try
+            {
+
+                return new ResponseSuccessAPI<DetailsProductDTO>(StatusCodes.Status200OK);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return new ResponseErrorAPI<DetailsProductDTO>(StatusCodes.Status500InternalServerError, "Lỗi hệ thống.");
+            }
+        }
+    }
+}
