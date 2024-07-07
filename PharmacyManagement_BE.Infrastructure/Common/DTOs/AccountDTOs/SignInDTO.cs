2471cd0d39b814b8b8560fbe6b470c514daa3ead
wwoi03 <tuandao8826848@gmail.com>
2024-07-07 23:39:27 +0700

    feat: update sign in admin

---
+++ PharmacyManagement_BE.Infrastructure/Common/DTOs/AccountDTOs/SignInDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PharmacyManagement_BE.Infrastructure.Common.DTOs.AccountDTOs
+{
+    public class SignInDTO
+    {
+        public string Name { get; set; }
+        public string Token { get; set; }
+        public List<string> Roles { get; set; }
+    }
+}
