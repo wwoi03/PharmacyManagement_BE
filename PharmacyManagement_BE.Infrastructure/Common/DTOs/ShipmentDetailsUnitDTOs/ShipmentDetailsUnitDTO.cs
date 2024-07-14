0c7258f6581973f8e7c6c463df0b79d81cbeec01
wwoi03 <tuandao8826848@gmail.com>
2024-07-14 19:11:44 +0700

    feat: update create product

---
+++ PharmacyManagement_BE.Infrastructure/Common/DTOs/ShipmentDetailsUnitDTOs/ShipmentDetailsUnitDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PharmacyManagement_BE.Infrastructure.Common.DTOs.ShipmentDetailsUnitDTOs
+{
+    public class ShipmentDetailsUnitDTO
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public decimal SalePrice { get; set; }
+    }
+}
