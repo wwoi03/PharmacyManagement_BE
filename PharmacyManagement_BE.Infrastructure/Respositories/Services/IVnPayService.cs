86cd90c7ee32fba1279c6c0f778972f39d018678
wwoi03 <tuandao8826848@gmail.com>
2024-07-24 17:01:30 +0700

    feat: update checkout vnpay

---
+++ PharmacyManagement_BE.Infrastructure/Respositories/Services/IVnPayService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using PharmacyManagement_BE.Infrastructure.Common.DTOs.PaymenDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PharmacyManagement_BE.Infrastructure.Respositories.Services
+{
+    public interface IVnPayService
+    {
+        Task<string> CreatePaymentUrl(PaymentInformationModel model, HttpContext context);
+        Task<PaymentResponseModel> PaymentExecute(IQueryCollection collections);
+    }
+}
